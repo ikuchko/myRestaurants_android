5ba2686abc56846a9a003dd5e4466638713a380e
Illia Kuchko and Midori Bowen <midori.p.bowen@gmail.com>
2016-03-14 10:07:42 -0700

    display toast on findRestaurantsButton click.

---
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -0,0 +1,25 @@
+package com.epicodus.myrestaurants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This is dog!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
