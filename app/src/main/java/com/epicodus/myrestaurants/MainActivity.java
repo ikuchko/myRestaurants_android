ec139f6e05da80fc3acb49bafa4f4b6b78d4ca1c
Illia Kuchko and Midori Bowen <ikuchko@gmail.com>
2016-03-15 22:04:03 -0700

    Add arrayadapter and refactor code using ButterKnife

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -1,6 +1,7 @@
 package com.epicodus.myrestaurants;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,21 +10,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
+    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
     public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i(TAG, "was clicked succesfully");
                 String location = mLocationEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
                 intent.putExtra("location", location);
--- app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
@@ -3,11 +3,15 @@ package com.epicodus.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private TextView mLocationTextView;
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro", "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina", "Lardo", "Portland City Grill", "Fat Head's Brewery", "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +19,9 @@ public class RestaurantsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
 
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
-
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the restaurants near: " + location);
